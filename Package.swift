378a64ec9dcdf7736b6b0bbef489e7ade24c2cad
Danielle Lancashire <dani@builds.terrible.systems>
2016-01-31 16:23:40 +0100

    Use DanielTomlinson/JSON 0.4.*

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
 						  .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0),
 						  .Package(url: "https://github.com/nestproject/Frank.git", majorVersion: 0),
 						  .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0),
-						  .Package(url: "https://github.com/endocrimes/JSON.git", majorVersion: 0)
+						  .Package(url: "https://github.com/endocrimes/JSON.git", majorVersion: 0, minor: 4)
 					  ],
 					  testDependencies: [
 						  .Package(url: "https://github.com/kylef/spectre-build", majorVersion: 0)
